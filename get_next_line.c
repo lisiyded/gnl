277c3fd5ebe67ee02896e64ba3b6b38ca26b3ab4
Shemika Powers <spowers@student.21-school.ru>
2020-07-06 18:18:50 +0300

    mygnl

---
+++ get_next_line.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spowers <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/16 23:29:28 by spowers           #+#    #+#             */
+/*   Updated: 2020/07/06 18:14:34 by spowers          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char    *ft_strcpy(char *dst, const char *src)
+{
+        int i;
+
+        i = 0;
+        while (src[i])
+        {
+                dst[i] = src[i];
+                i++;
+        }
+        dst[i] = '\0';
+        return (dst);
+}
+
+char	*check_ost(char *ost, char **line)
+{
+	char *p_n;
+
+	p_n = NULL;
+	if (ost)
+		if ((p_n = ft_strchr(ost, '\n'))) // новая линия нашлась
+		{
+			*p_n = '\n';
+			*line = ft_strdup(ost);
+			ft_strcpy(ost, ++p_n);
+		}
+		else
+		{
+			*line = ft_strdup(ost);
+			if (*ost)
+				while (*ost)
+				{
+					*ost= '\0';
+					ost++;
+				}	
+		}
+	else
+		*line = ft_strnew(1);
+	return(p_n);
+}	
+
+int	get_next_line(int fd, char **line) // 1, 0, -1
+{
+	char buf[BUFF_SIZE + 1];
+	int bytes;
+	char *p_n;
+	static char *ost;
+	char *tmp;
+
+	if (fd < 0 || !line || BUFF_SIZE <= 0)
+		return (-1);
+	if (!(*line = ft_strdup(buf)))
+		return (-1);
+
+	p_n = check_ost(ost, line);
+	while (!p_n && (bytes = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[bytes] = '\0';
+		if ((p_n = ft_strchr(buf, '\n')))
+		{
+			*p_n = '\0';
+			p_n++;
+			ost = ft_strdup(p_n);
+		}
+		tmp = *line;
+		*line = ft_strjoin(*line, buf);
+		free(tmp);
+	}
+	return (bytes || ft_strlen(ost) || ft_strlen(*line)) ? 1 : 0;
+}
+
+int     main(void)
+{
+char    *line;
+int     fd;
+
+fd = open("text.txt", O_RDONLY);
+get_next_line(fd, &line);
+printf("%s\n\n", line);
+
+get_next_line(fd, &line);
+printf("%s\n\n", line);
+
+get_next_line(fd, &line);
+printf("%s\n\n", line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spowers <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/15 22:51:01 by spowers           #+#    #+#             */
+/*   Updated: 2020/07/06 17:07:33 by spowers          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strnew(size_t size)
+{
+	char *str;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (str == NULL)
+		return (NULL);
+	while (size > 0)
+		str[size--] = '\0';
+	str[0] = '\0';
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char		*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
