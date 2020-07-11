dcb7d285224a267a2cf9e86a86213f24f63c564b
Shemika Powers <spowers@student.21-school.ru>
2020-07-11 04:33:54 +0300

    razbor

---
+++ test_get.c
@@ -0,0 +1,100 @@
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t          ft_strlen(const char *s)
+{
+        size_t  len;
+
+        len = 0;
+        while (s[len])
+                len++;
+        return (len);
+}
+
+char            *ft_strdup(const char *s1)
+{
+        char    *str;
+        size_t  i;
+
+        i = 0;
+        str = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+        if (str == NULL)
+                return (NULL);
+        while (s1[i])
+        {
+                str[i] = s1[i];
+                i++;
+        }
+        str[i] = '\0';
+        return (str);
+}
+
+char            *ft_strjoin(char const *s1, char const *s2)
+{
+        int             i;
+        int             j;
+        char    *str;
+
+        i = 0;
+        j = 0;
+        if (s1 == NULL || s2 == NULL)
+                return (NULL);
+        str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+        if (str == NULL)
+                return (NULL);
+        while (s1[i])
+        {
+                str[i] = s1[i];
+                i++;
+        }
+        while (s2[j])
+        {
+                str[i] = s2[j];
+                i++;
+                j++;
+        }
+        str[i] = '\0';
+        return (str);
+}
+
+// в line записано strdup "" чтобы строка была без мусора
+// в цикле происходит полное чтение файла txt с помощью записи функции read
+// в переменную butes_was_read
+// массив записывается по 10 символов в лайн при помощи склеивания строк (strjoin)
+// после последнего прочитанного байта ставится \0
+
+int	get_next_line(int fd, char **line)
+{
+	char buf[10 + 1];
+	int bytes_was_read;
+	
+	
+	*line = strdup("");
+	while ((bytes_was_read = read(fd, &buf, 10)))
+	{
+	buf[bytes_was_read] = '\0';
+	*line = ft_strjoin(*line, buf);
+	}
+	return (0);
+} 
+
+
+int     main(void)
+{
+char    *line;
+int     fd;
+int     res;
+
+fd = open("text.txt", O_RDONLY);
+while ((res = get_next_line(fd, &line)))
+{
+        printf("res = %d, line is: %s\n", res, line);
+        free(line);
+}
+
+printf("res = %d, line is:%s\n ",res, line);
+
+return(0);
+}
